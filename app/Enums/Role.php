a8dea60f8ad3654e64cd6efebc25e186d36c6101
rafa14494 <rafa14494@gmail.com>
2024-06-05 09:07:41 +0000

    security: permisos en productos y usuarios

--- app/Enums/Role.php
+++ app/Enums/Role.php
@@ -6,4 +6,12 @@ enum Role: string
 {
     case USER = 'USER';
     case ADMIN = 'ADMIN';
+
+    public function getValue(): string
+    {
+        return match ($this) {
+            self::USER => 'USUARIO',
+            self::ADMIN => 'ADMINISTRADOR'
+        };
+    }
 }
