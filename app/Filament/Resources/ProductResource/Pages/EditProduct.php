5fdbe788710fdccefdcda05aa90576710e437ef9
rafa14494 <rafa14494@gmail.com>
2024-06-09 14:26:18 +0000

    fix: redireccionar al index de productos al actualizar un producto

--- app/Filament/Resources/ProductResource/Pages/EditProduct.php
+++ app/Filament/Resources/ProductResource/Pages/EditProduct.php
@@ -10,6 +10,11 @@ class EditProduct extends EditRecord
 {
     protected static string $resource = ProductResource::class;
 
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+
     protected function getHeaderActions(): array
     {
         return [
--- app/Services/OrderService.php
+++ app/Services/OrderService.php
@@ -3,19 +3,22 @@
 namespace App\Services;
 
 use App\Models\Order;
+use Exception;
+use Illuminate\Database\Eloquent\Collection;
+
 
 class OrderService
 {
-    public function list()
+    public function list(): Collection
     {
         return Order::query()->get();
     }
 
-    public function find(string $uid)
+    public function find(string $uid): object
     {
         $order = Order::query()->where('uid', 'LIKE', '%'.$uid.'%')->first();
         if (!$order) {
-            throw new Exception(['message' => 'No se encontraron resultados'], 404);
+            throw new Exception('No se encontraron resultados', 404);
         }
         return $order;
     }
