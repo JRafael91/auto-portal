82f0c2ca6c3597adda4ebc847f959317f29ad31c
Rafael Acosta <rafael@tecnofy.org>
2024-06-09 05:53:25 -0700

    test agregados

--- app/Filament/Pages/Dashboard.php
+++ app/Filament/Pages/Dashboard.php
@@ -1,10 +1,12 @@
 <?php
- 
+
 namespace App\Filament\Pages;
- 
+
 class Dashboard extends \Filament\Pages\Dashboard
 {
     protected static ?string $title = 'Dashboard';
+
+    protected static string $routePath = 'dashboard';
 
     protected static ?string $navigationIcon = 'heroicon-o-presentation-chart-line';
 }
